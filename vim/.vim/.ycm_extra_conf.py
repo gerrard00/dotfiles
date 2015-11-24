463d95ad89da029f8e1302be95f649bc9531df04
Gerrard Lindsay <gerrard@pickabar.com>
2015-11-23 20:42:30 -0500

    update ycm settings.

--- vim/.vim/.ycm_extra_conf.py
+++ vim/.vim/.ycm_extra_conf.py
@@ -19,17 +19,18 @@ flags = [
     # a "-std=<something>".
     # For a C project, you would set this to something like 'c99' instead of
     # 'c++11'.
-    '-std=c++11',
+    '-std=c11',
     # ...and the same thing goes for the magic -x option which specifies the
     # language that the files to be compiled are written in. This is mostly
     # relevant for c++ headers.
     # For a C project, you would set this to 'c' instead of 'c++'.
-    '-x', 'c++',
+    '-x', 'c',
     # This path will only work on OS X, but extra paths that don't exist are not
     # harmful
     '-isystem', '/System/Library/Frameworks/Python.framework/Headers',
     '-isystem', '/usr/local/include',
-    '-isystem', '/usr/local/include/eigen3',
+    '-isystem', '/usr/include',
+    '-isystem', '/usr/include/glib-2.0',
     '-I', 'include'
     '-I.'
 ]
