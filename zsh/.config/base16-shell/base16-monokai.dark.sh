0b4c8d2132549453e2c2ebb19b326b7827316538
Gerrard Lindsay <gerrardalindsay@gmail.com>
2016-01-30 19:53:56 -0500

    zsh: base 16 shell script shouldn't set bg, breaks transparency.

--- zsh/.config/base16-shell/base16-monokai.dark.sh
+++ zsh/.config/base16-shell/base16-monokai.dark.sh
@@ -88,7 +88,8 @@ if [ -n "$ITERM_SESSION_ID" ]; then
   printf $printf_template_custom Pm 272822 # cursor text
 else
   printf $printf_template_var 10 $color_foreground
-  printf $printf_template_var 11 $color_background
+
+  # printf $printf_template_var 11 $color_background
   printf $printf_template_var 12 $color_cursor
 fi
 
