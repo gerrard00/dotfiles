b330adb225e88b03cff69ade66743f6db4d25803
Gerrard Lindsay <gerrardalindsay@gmail.com>
2016-02-04 12:27:02 -0500

    tmux: set work alias to detach existing clients. iterm2/zprezto workaround. Also, re-enable use of session name in stopwork.

--- tmux/.aliases_local/tmuxinator_helpers.sh
+++ tmux/.aliases_local/tmuxinator_helpers.sh
@@ -5,12 +5,19 @@ function _get_session_name()
 
 function work()
 {
-  tmuxinator start node -n $(_get_session_name)
+  let session_name=$(_get_session_name)
+
+  # kill any already attached clients
+  # workaround for the fact that iterm2 can't detach from sessions
+  # on window close since zprezto sets nohup
+  tmux detach-client  -s $session_name
+  # attach or start the named session
+  tmuxinator start node -n $session_name
 }
 
 function stopwork()
 {
   # kill the current session
-  tmux kill-session # -t $(_get_session_name)  
+  tmux kill-session -t $(_get_session_name)  
   exit
 }
