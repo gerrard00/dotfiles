74260a56e753e6b576dc5c7a068016223fe39a1a
Gerrard Lindsay <gerrard.lindsay@kaplan.com>
2016-08-11 13:42:06 -0400

    postrgres: make auto execution function overwrite output file.

--- postgresql/.aliases_local/pgsql.sh
+++ postgresql/.aliases_local/pgsql.sh
@@ -3,6 +3,6 @@ function pgsql_execfile()
   clear
   echo "\n"
   tmpfile="$(mktemp).html"
-  nodemon --exec "psql -U postgres -h localhost -p 5433 -d $1 -f $2 --html >> $tmpfile && w3m -dump $tmpfile && echo "
+  nodemon --exec "psql -U postgres -h localhost -p 5433 -d $1 -f $2 --html -- > $tmpfile && w3m -dump $tmpfile && echo " $2
 
 }
