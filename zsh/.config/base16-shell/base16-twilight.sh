cedc45c83901e98e9b9e1b47e6f390272016eb0c
Gerrard Lindsay <gerrardalindsay@gmail.com>
2016-09-09 21:53:05 -0400

    get transparency working again with twilight.

--- zsh/.config/base16-shell/base16-twilight.sh
+++ zsh/.config/base16-shell/base16-twilight.sh
@@ -89,7 +89,8 @@ if [ -n "$ITERM_SESSION_ID" ]; then
   printf $printf_template_custom Pm 1e1e1e # cursor text
 else
   printf $printf_template_var 10 $color_foreground
-  printf $printf_template_var 11 $color_background
+  # gerrard: comment this so we can have urxvt true transparency
+  # printf $printf_template_var 11 $color_background
   printf $printf_template_custom 12 ";7" # cursor (reverse video)
 fi
 
