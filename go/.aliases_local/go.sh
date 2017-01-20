e57f7fc8a6405e71461356d7a10de1c8cac8712e
Gerrard Lindsay <gerrardalindsay@gmail.com>
2017-01-20 16:11:47 -0500

    go: make gosrccd alias and add go bin to path

--- go/.aliases_local/go.sh
+++ go/.aliases_local/go.sh
@@ -4,10 +4,12 @@ function gocd() {
   cd $GOPATH/$1
 }
 
-function gowscd() {
+function gosrccd() {
   cd $GOPATH/src/github.com/gerrard00/$1
 }
 
 # set gopath here, so it's only set
 # on machine where I use go
 export GOPATH=~/go
+export PATH=$PATH:$GOPATH/bin
+
