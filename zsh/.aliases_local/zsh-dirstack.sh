5639af6c613f4280a1d869aa4448f34ee6923dfd
Gerrard Lindsay <gerrardalindsay@gmail.com>
2017-10-22 14:22:05 -0400

    zsh: some more dir bookmark stuff, unbookmark

--- zsh/.aliases_local/zsh-dirstack.sh
+++ zsh/.aliases_local/zsh-dirstack.sh
@@ -15,3 +15,16 @@ bookmark() {
   echo $PWD >> $tmpfile
 	sort $tmpfile | uniq  >! $DIRBOOKMARKSFILE
 }
+
+bookmark() {
+	local tmpfile=$(mktemp /tmp/XXXzdirs_tempXXX)
+  cat $DIRBOOKMARKSFILE >! $tmpfile
+  echo $PWD >> $tmpfile
+	sort $tmpfile | uniq  >! $DIRBOOKMARKSFILE
+}
+
+unbookmark() {
+	local tmpfile=$(mktemp /tmp/XXXzdirs_tempXXX)
+  cat $DIRBOOKMARKSFILE | grep -v "^$PWD$"  >! $tmpfile
+	mv $tmpfile $DIRBOOKMARKSFILE
+}
