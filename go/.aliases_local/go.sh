d82560068a0de5e3a9cdf2845a86afd5bdc4d203
Gerrard Lindsay <gerrardalindsay@gmail.com>
2017-10-22 20:19:56 -0400

    go: get rid of custom cd functions

--- go/.aliases_local/go.sh
+++ go/.aliases_local/go.sh
@@ -1,13 +1,3 @@
-
-#setup gocd to go home
-function gocd() {
-  cd $GOPATH/$1
-}
-
-function gosrccd() {
-  cd $GOPATH/src/github.com/gerrard00/$1
-}
-
 # set gopath here, so it's only set
 # on machine where I use go
 export GOPATH=~/go
