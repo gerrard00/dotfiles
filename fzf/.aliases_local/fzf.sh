d598732dde5f50c5e9a223141c3fe786e53130fb
Gerrard Lindsay <gerrardalindsay@gmail.com>
2019-09-29 21:10:49 -0400

    fzf: add alias fd for searching for directories

--- fzf/.aliases_local/fzf.sh
+++ fzf/.aliases_local/fzf.sh
@@ -44,3 +44,10 @@ cdb() {
   [[ -n "$dir" ]] && cd $dir
 }
 
+# fd - cd to selected directory
+fd() {
+  local dir
+  dir=$(find ${1:-.} -path '*/\.*' -prune \
+                  -o -type d -print 2> /dev/null | fzf +m) &&
+  cd "$dir"
+}
