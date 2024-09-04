c105753ecfdab1814bea999c6c9ba883008764fa
Gerrard Lindsay <gerrardalindsay@gmail.com>
2024-09-04 11:31:25 -0400

    nvim: my custom open and split mappings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,6 @@
-require("config.lazy")
+vim.g.mapleader = "\\"
 
+require("config.lazy")
 
 vim.opt.number = true
 
@@ -12,11 +13,22 @@ vim.opt.shiftwidth = 2
 -- Use spaces instead of tabs
 vim.opt.expandtab = true
 
+
 vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true, silent = true })
+
+-- Open a file in the same directory as the current buffer
+vim.api.nvim_set_keymap('n', ',e', ':e <C-R>=fnamemodify(expand("%:h:p"), ":~:.") . "/" <CR>', { noremap = true, silent = false })
+-- Open a horizontal split in the same directory as the current buffer
+vim.api.nvim_set_keymap('n', ',s', ':split <C-R>=fnamemodify(expand("%:p:h"), ":~:.") . "/" <CR>', { noremap = true, silent = false })
+-- Open a new tab in the same directory as the current buffer
+vim.api.nvim_set_keymap('n', ',t', ':tabe <C-R>=fnamemodify(expand("%:p:h"), ":~:.") . "/" <CR>', { noremap = true, silent = false })
+-- Open a vertical split in the same directory as the current buffer
+vim.api.nvim_set_keymap('n', ',v', ':vsplit <C-R>=fnamemodify(expand("%:p:h"), ":~:.") . "/" <CR>', { noremap = true, silent = false })
 
 --[[ 
 TODO:
 
 * luarocks
-
+* replace vim sideways with sibling-swap.nvim?
+* replace abolish with textcase?
 --]]
