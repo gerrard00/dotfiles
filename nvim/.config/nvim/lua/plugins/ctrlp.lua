93aed0b0e1c4d3c0ad104f87a6702fb89bc8de3d
Gerrard Lindsay <gerrardalindsay@gmail.com>
2024-10-22 13:18:22 -0400

    nvim: ctrlp for now

--- nvim/.config/nvim/lua/plugins/ctrlp.lua
+++ nvim/.config/nvim/lua/plugins/ctrlp.lua
@@ -5,10 +5,11 @@ return {
     { "<C-p>", ":CtrlP<CR>", desc = "CtrlP fuzzy finder" }
   },
   config = function()
-    -- Equivalent of: let g:ctrlp_user_command = ['.git', 'cd %s && git ls-files -co --exclude-standard']
-    vim.g.ctrlp_user_command = { '.git', 'cd %s && git ls-files -co --exclude-standard' }
+    vim.g.ctrlp_user_command = 'ag -l -g ""'
 
     -- Equivalent of: let g:ctrlp_use_caching = 0
     vim.g.ctrlp_use_caching = 0
+
+    vim.g.ctrlp_cmd = 'CtrlPBuffer'
   end
 }
