01d123b7f27ce761fc443ded40538aaf6f6749a5
Gerrard Lindsay <gerrardalindsay@gmail.com>
2025-11-03 17:55:22 -0500

    nvim: mason stuff

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -11,13 +11,13 @@ return {
     },
     config = function()
       require('mason-lspconfig').setup({
-        ensure_installed = {'ts_ls'},
+        ensure_installed = {'ts_ls', 'eslint'},
         handlers = {
           -- this first function is the "default handler"
           -- it applies to every language server without a "custom handler"
           function(server_name)
             -- Only set up servers that aren't already configured
-            if server_name ~= 'ts_ls' then
+            if server_name ~= 'ts_ls' and server_name ~= 'eslint' then
               local capabilities = vim.tbl_deep_extend(
                 'force',
                 vim.lsp.protocol.make_client_capabilities(),
