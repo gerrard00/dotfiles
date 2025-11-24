534590370c45f1f5e2a27ef0c0a2bc190812e275
Gerrard Lindsay <gerrardalindsay@gmail.com>
2025-11-24 09:25:14 -0500

    postgres: updates for new job

--- shells/.aliases_local/postgres.sh
+++ shells/.aliases_local/postgres.sh
@@ -1,9 +1,16 @@
 postgres_log_all() {
+  local container_name="postgres-15"
   local conf_file="/var/lib/postgresql/data/postgresql.conf"
-  local grep_result=$(docker-compose exec -T postgres bash -c "grep -qxF \"log_statement = 'all'\" $conf_file")
+
+  echo "Checking if log_statement is already set to 'all' in $container_name..."
+
+  # Check whether it's already set
+  local grep_result
+  grep_result=$(docker exec -i "$container_name" bash -c "grep -qxF \"log_statement = 'all'\" $conf_file" 2>/dev/null)
 
   if [[ -z "$grep_result" ]]; then
-    docker-compose exec -T postgres bash -c "echo \"log_statement = 'all'\" >> $conf_file && kill -s SIGHUP 1"
+    echo "Enabling full SQL statement logging..."
+    docker exec -i "$container_name" bash -c "echo \"log_statement = 'all'\" >> $conf_file && kill -s SIGHUP 1"
   else
     echo "log_statement is already set to 'all'. Skipping modification."
   fi
