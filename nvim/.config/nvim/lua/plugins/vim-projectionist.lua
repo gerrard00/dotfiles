9f0c4c32847e2d29f201ea958e5444303b91bcaf
Gerrard Lindsay <gerrardalindsay@gmail.com>
2026-02-12 17:00:22 -0500

    nvim: projectionist

--- nvim/.config/nvim/lua/plugins/vim-projectionist.lua
+++ nvim/.config/nvim/lua/plugins/vim-projectionist.lua
@@ -1,3 +1,26 @@
-return  {
-    "tpope/vim-projectionist"
-  }
+return {
+  "tpope/vim-projectionist",
+  event = { "BufReadPre", "BufNewFile" },
+  init = function()
+    vim.g.projectionist_heuristics = vim.tbl_deep_extend(
+      "force",
+      vim.g.projectionist_heuristics or {},
+      {
+        ["src/"] = {
+          ["src/*.ts"] = {
+            alternate = {
+              "tests/unit/{}.spec.ts",
+              "tests/e2e/{}.spec.ts",
+            },
+          },
+          ["tests/unit/*.spec.ts"] = {
+            alternate = "src/{}.ts",
+          },
+          ["tests/e2e/*.spec.ts"] = {
+            alternate = "src/{}.ts",
+          },
+        },
+      }
+    )
+  end,
+}
